65cf66bc76baf5c97b08a005e85cb2bfc5ddde99
Alvin Ochutsi <tetalvin@gmail.com>
2025-09-29 10:09:51 +0300

    Modified Schema to have relevant fields after data clean up

--- priv/repo/migrations/20250924102714_create_customers.exs
+++ priv/repo/migrations/20250924102714_create_customers.exs
@@ -3,23 +3,18 @@ defmodule Pagination.Repo.Migrations.CreateCustomers do
 
   def change do
     create table(:customers) do
-      add :index, :integer, null: false
-      add :customer_id, :string, null: false
       add :first_name, :string
       add :last_name, :string
-      add :company, :string
-      add :city, :string
       add :country, :string
-      add :phone_1, :string
-      add :phone_2, :string
       add :email, :string
       add :subscription_date, :date
-      add :website, :string
 
       timestamps()
     end
 
-    create unique_index(:customers, [:index])
-    create index(:customers, [:subscription_date, :index])
+    create index(:customers, [:customer_id])
+    create index(:customers, [:email])
+    create index(:customers, [:country])
+    create index(:customers, [:subscription_date])
   end
 end
