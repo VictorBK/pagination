7852a0c506ad77f3d55626b3eb0584683b68ed49
Alvin Ochutsi <tetalvin@gmail.com>
2025-09-29 12:16:26 +0300

    Build page response defined

--- lib/pagination/customers.ex
+++ lib/pagination/customers.ex
@@ -17,11 +17,30 @@ defmodule Pagination.Customers do
   def list_customers(page_size \\ @default_page_size) do
     query =
       from c in Customer,
-      where: c.id > ^cursor_id,
-      order_by: [desc: c.id],
+      order_by: [asc: c.id],
       limit: ^(page_size + 1)
 
       customers = Repo.all(query)
       build_page_response(customers, page_size)
   end
+
+  #page response helper
+  defp build_page_response(customers, page_size) do
+    has_more = length(customers) > page_size
+    data = if has_more, do: Enum.take(customers, page_size), else: customers
+
+  %{
+    data: data,
+    has_more: has_more,
+    next_cursor: get_next_cursor(data),
+    prev_cursor: get_prev_cursor(data),
+    count: length(data)
+  }
+end
+
+defp get_next_cursor([]), do: nil
+  defp get_next_cursor(customers), do: List.last(customers).id
+
+  defp get_prev_cursor([]), do: nil
+  defp get_prev_cursor(customers), do: List.first(customers).id
 end
