c7687eb2399e237ad368bae7e2aa9007731fd4a0
VictorBK <victor.kanyangah@gmail.com>
2025-09-30 08:27:10 +0300

    modification of alias to Pagination.Customers.Customer to ensure module compilation

--- lib/pagination/customers.ex
+++ lib/pagination/customers.ex
@@ -3,10 +3,9 @@ defmodule Pagination.Customers do
   Context for managing Customers and Pagination.
   """
 
-
   import Ecto.Query
   alias Pagination.Repo
-  alias Pagination.Customer
+  alias Pagination.Customers.Customer
 
   @default_page_size 20
 
@@ -17,28 +16,28 @@ defmodule Pagination.Customers do
   def list_customers(page_size \\ @default_page_size) do
     query =
       from c in Customer,
-      order_by: [asc: c.id],
-      limit: ^(page_size + 1)
+        order_by: [asc: c.id],
+        limit: ^(page_size + 1)
 
-      customers = Repo.all(query)
-      build_page_response(customers, page_size)
+    customers = Repo.all(query)
+    build_page_response(customers, page_size)
   end
 
-  #page response helper
+  # page response helper
   defp build_page_response(customers, page_size) do
     has_more = length(customers) > page_size
     data = if has_more, do: Enum.take(customers, page_size), else: customers
 
-  %{
-    data: data,
-    has_more: has_more,
-    next_cursor: get_next_cursor(data),
-    prev_cursor: get_prev_cursor(data),
-    count: length(data)
-  }
-end
+    %{
+      data: data,
+      has_more: has_more,
+      next_cursor: get_next_cursor(data),
+      prev_cursor: get_prev_cursor(data),
+      count: length(data)
+    }
+  end
 
-defp get_next_cursor([]), do: nil
+  defp get_next_cursor([]), do: nil
   defp get_next_cursor(customers), do: List.last(customers).id
 
   defp get_prev_cursor([]), do: nil
