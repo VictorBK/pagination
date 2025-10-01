15e0ee2d5f4b564f9de3a09c14c833da0e980ad1
scorpioFelix <fyllikx@gmail.com>
2025-10-01 10:30:15 +0300

    test to return customers with pagination info added

--- test/pagination/customers_test.exs
+++ test/pagination/customers_test.exs
@@ -2,13 +2,21 @@ defmodule Pagination.CustomersTest do
 use Pagination.DataCase
 alias Pagination.Customers
 
-  describe "list_customers/1" do
+describe "list_customers/1" do
     test "returns empty result when no customers" do
       result = Customers.list_customers()
 
       assert result.data == []
       assert result.has_more == false
       assert result.next_cursor == nil
+    end
+
+    test "returns customers with Pagination info" do
+      customers_fixture(5)
+
+      result = Customers.list_customers(3)
+
+      assert length(result.data) == 3
     end
 
   end
